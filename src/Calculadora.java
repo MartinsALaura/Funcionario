d5d042dcdff0fdc438bc4f23c8ffbabe381372c7
MartinsALaura <lauraalvesmartins04@gmail.com>
2024-05-25 14:54:22 -0300

    Create new class Calculadora

---
+++ src/Calculadora.java
@@ -0,0 +1,55 @@
+public class Calculadora {
+    private double memoria;
+    private String cor;
+    
+    public Calculadora(String cor) {
+        this.cor = cor;
+        this.memoria = 0;
+    }
+    
+    public double getMemoria() {
+        return memoria;
+    }
+
+    public void setMemoria(double memoria) {
+        this.memoria = memoria;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double soma(int a, int b){
+        return a + b;
+    }
+
+    public double subtrai(int a, int b){
+        return a + b;
+    }
+
+    public double multiplica(int a, int b){
+        return a * b;
+    }
+
+    public double divide(int a, int b){
+        return a / b;
+    }
+
+    public double elevaAoQuadrado(int a){
+        return a * a;
+    }
+
+    public double elevaAoCubo(int a){
+        return a * a * a;
+    }
+
+    public void imprimeInfo() {
+        System.out.println("#### Informações : ####");
+        System.out.println("Cor: " + cor + "\nMemória: " + memoria);
+        System.out.println("Operações: soma, subtração, divisão, multiplicação, elevação ao quadrado e ao cubo.");
+    }
+}
